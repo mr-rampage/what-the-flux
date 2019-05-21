1ba9e60cf9731bfbde496be44ce3357637a2024d
mr-rampage <readonly@che.wbac.ca>
2019-05-21 15:57:14 -0400

    feat(seed)

--- wikipedia.flux.js
+++ wikipedia.flux.js
@@ -1,30 +1,6 @@
-const SEARCH_WIKIPEDIA_ACTION = Symbol('Search Wikipedia');
+/*
+fetch(`http://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=${action.payload}`)
+  .then(response => response.json())
+  .then(setWikiAction);
+*/
 
-function searchWikiAction(payload) {
-  return {
-    'type': SEARCH_WIKIPEDIA_ACTION,
-    payload
-  }
-}
-
-function searchWikiSideEffect(state, action) {
-  return fetch(`http://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=${action.payload}`)
-    .then(response => response.json())
-    .then(setWikiAction);
-}
-
-const UPDATE_SEARCH_RESULTS_ACTION = Symbol('Update Wikipedia results');
-
-function setWikiAction(payload) {
-  return {
-    'type': UPDATE_SEARCH_RESULTS_ACTION,
-    payload
-  };
-}
-
-function setWikiReducer(state, action) {
-  return {
-    ...state,
-    'wiki': action.payload
-  };
-}
